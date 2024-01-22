f4990cbd7c88f2a6bf9b70c0df2c36f0a1ff1415
Kshitij2914 <96278409+Kshitij2914@users.noreply.github.com>
2024-01-22 12:00:43 +0530

    Update ResetPassword.jsx

--- client/src/ResetPassword.jsx
+++ client/src/ResetPassword.jsx
@@ -11,7 +11,7 @@ function ResetPassword() {
 
     function handleSubmit(e) {
         e.preventDefault();
-        axios.post(`http://localhost:3001/reset_password/${id}/${token}`, { password })
+        axios.post(`https://secrate-message.vercel.app/reset_password/${id}/${token}`, { password })
             .then(result => {
                 if (result.data === "Success") {
                     navigate('/login')
